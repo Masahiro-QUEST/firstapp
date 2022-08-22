c37dd07e61026920a068d76c16369fcdd748b1bc
MASAHIRO.YAMADA <masahiro.yamada@quest.co.jp>
2022-08-22 16:56:05 +0900

    以下の処理を追加 1.Firebase Storageに画像をアップロード 2.本一覧に画像を表示するUIの追加

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -65,6 +65,8 @@ class BookListPage extends StatelessWidget {
 
                     ),
                     child: ListTile(
+                      leading: book.imgURL != null
+                          ? Image.network(book.imgURL!) : null,
                       title: Text(book.title),
                       subtitle: Text(book.author),
                     ),
